e0a45f2ea28fbc5407a49e9c09b2ad031cc7bd57
May Chen <may@Mays-MacBook-Pro.local>
2019-07-28 10:25:49 +1000

    processDialog function

---
+++ index.js
@@ -0,0 +1,8 @@
+const { processDialog } = require("./processDialog");
+
+processDialog("./data/bladerunner_partial.txt", {
+  id: "000001",
+  title: "Blade Runner",
+  director: "Ridley Scott",
+  year: "1982"
+})
--- processDialog.js
+++ processDialog.js
@@ -1,3 +1,94 @@
-module.exports = function () {
+const fs = require("fs");
+const readline = require("readline");
+const stream = require("stream");
+const uuidv4 = require("uuid/v4");
+const parenthesisRegex = new RegExp(/(?=\().+?(?<=\))/g);
+const spacesRegex = new RegExp(/[ ]{2,}/g);
+const spaceRegex = new RegExp(/[ ]{1,}/g);
+const sanitiseLine = line => {
+  // remove all the content in parenthesis
+  line = line.replace(parenthesisRegex, "");
+  // replace more than 1 space with just 1 space
+  line = line.replace(spacesRegex, " ");
+  return line;
+};
+const sanitiseCharacter = character => {
+  // remove all the content in parenthesis
+  character = character.replace(parenthesisRegex, "");
+  character = character.replace(spaceRegex, "");
+  return character;
+};
+
+exports.processDialog = (filePath, filmInfo) => {
+  const instream = fs.createReadStream(filePath);
+  const outstream = new stream();
+  const rl = readline.createInterface(instream, outstream);
+  const characterRegex = new RegExp(/^[A-Z '().]*$/);
+  let currentCharacter;
+  let currentLine = "";
+  let currentDialog;
+  let prevDialog;
+  let dialogs = [];
+  let isFirstLine = true;
+  rl.on("line", line => {
+    if (line) {
+      if (characterRegex.test(line)) {
+        // when the line is a character
+        if (!isFirstLine) {
+          currentLine = sanitiseLine(currentLine);
+          currentDialog = {
+            dialogId: `${filmInfo.id}-${uuidv4()}`,
+            dialog: currentLine ? currentLine : "null",
+            character: currentCharacter ? currentCharacter : "n/a",
+            filmId: filmInfo.id,
+            film: filmInfo.title,
+            director: filmInfo.director,
+            year: filmInfo.year
+          };
+          if (prevDialog) {
+            currentDialog.prevId = prevDialog.dialogId;
+            prevDialog.nextId = currentDialog.dialogId;
+            dialogs.push(prevDialog);
+            if (dialogs.length > 20) {
+              addDialogsBatch(dialogs);
+              dialogs = [];
+            }
+          }
+          prevDialog = currentDialog;
+        } else {
+          isFirstLine = false;
+        }
+        currentCharacter = sanitiseCharacter(line);
+        currentLine = "";
+      } else {
+        // when the line is a dialog
+        currentLine += `${line} `;
+      }
+    }
+  });
+  rl.on("close", () => {
+    // push the last line
+    currentLine = sanitiseLine(currentLine);
+    currentDialog = {
+      dialogId: `${filmInfo.id}-${uuidv4()}`,
+      dialog: currentLine,
+      character: currentCharacter,
+      filmId: filmInfo.id,
+      film: filmInfo.title,
+      director: filmInfo.director,
+      year: filmInfo.year
+    };
+    if (prevDialog) {
+      currentDialog.prevId = prevDialog.dialogId;
+      prevDialog.nextId = currentDialog.dialogId;
+      dialogs.push(prevDialog);
+    }
+    dialogs.push(currentDialog);
+    console.log("finished");
+    const dialogsJson = JSON.stringify(dialogs);
+    fs.writeFile(`${filePath}_processed.json`, dialogsJson, err => {
+      if (err) throw err;
+    });
+  });
 
 }
